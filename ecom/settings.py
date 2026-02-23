4282cc6f1315ea6f8685a260b6e4e29d55740a79
bthinkxdev <bthinkx@gmail.com>
2026-02-23 08:55:28 +0530

    db name updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'queenorange_db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # DATABASES = {
